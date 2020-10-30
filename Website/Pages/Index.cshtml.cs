3c680b7166da9cf6723ab48393053b499f2697ca
Elliot Peters <ewpeters@ksu.edu>
2020-10-30 17:06:27 -0500

    Done With milestone

---
+++ Website/Pages/Index.cshtml.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Elliot Peters
+ * Index.cshtml.cs
+ * Creating backing variables to pull from the for index html
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Sides;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+       
+        }
+
+        /// <summary>
+        /// Object for entree menu items
+        /// </summary>
+        public IEnumerable<IOrderItem> Entrees
+        {
+            get
+            {
+                return Menu.Entrees();
+            }
+        }
+
+        /// <summary>
+        /// Object for side menu items
+        /// </summary>
+        public IEnumerable<IOrderItem> Sides
+        {
+            get
+            {
+                return Menu.Sides();
+            }
+        }
+
+        /// <summary>
+        /// Object for drink menu items
+        /// </summary>
+        public IEnumerable<IOrderItem> Drinks
+        {
+            get
+            {
+                return Menu.Drinks();
+            }
+        }
+    }
+}
